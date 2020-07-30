e5da97db7b3d9a910935e3ed395c7b62cb9ed297
Joel Haker <joeladam@gmail.com>
2020-07-30 02:10:36 -0400

    updates to the desktop script

--- debian/desktop.sh
+++ debian/desktop.sh
@@ -5,6 +5,8 @@ if ! ([ -n "$CWD" ] && [ -n "$platform" ] && [ "$provision_type" = "desktop" ]);
     exit 1
 fi
 
+#----------------------------------------------------------------------------------------------------
+
 ###
 #   This script was created to remind me of all the things I like to
 #   install on my Ubuntu desktop.
@@ -34,88 +36,93 @@ fi
 #   white dark   = #D7D7D7   white light    = #ffffff
 ###
 
-##------------------------------------------------------------------------------------------------##
+#----------------------------------------------------------------------------------------------------
+# Start Script
 
-## Start Script
-
-msg_c -c "Update, Upgrade, and Autoremove"
-sudo apt-get update && sudo apt-get -y upgrade && sudo apt autoremove
+msg_c -c "apt-get update and upgrade"
+#----------------------------------------------------------------------------------------------------
+apt-update-upgrade
+#----------------------------------------------------------------------------------------------------
 msg_c -c "Done!"
 
-msg_c -c "Install restricted extras"
+msg_c -c "Installing Fun stuff"
+#----------------------------------------------------------------------------------------------------
+sudo apt install -y software-properties-common build-essential
+#----------------------------------------------------------------------------------------------------
+msg_c -c "Done!"
+
+msg_c -c "Installing restricted extras"
+#----------------------------------------------------------------------------------------------------
 sudo apt install ubuntu-restricted-extras ubuntu-restricted-addons
+#----------------------------------------------------------------------------------------------------
 msg_c -c "Done!"
 
-msg_c -c "Install utility applications"
-sudo apt-get install -y git vim-gnome tmux curl tilix htop tree mysql-client
+msg_c -c "Installing utility applications"
+#----------------------------------------------------------------------------------------------------
+sudo apt install -y gnome-tweak-tool git vim-gtk3 tmux curl tilix htop tree mysql-client mosquitto-clients sshfs
+#----------------------------------------------------------------------------------------------------
 msg_c -c "Done!"
 
-msg_c -c "Install the sshfs"
-sudo apt-get install -y sshfs
+msg_c -c "Installing the ability to work with exfat drives"
+#----------------------------------------------------------------------------------------------------
+sudo apt install -y exfat-utils exfat-fuse
+#----------------------------------------------------------------------------------------------------
 msg_c -c "Done!"
 
-msg_c -c "Install gnome tweak tool and compiz"
-sudo apt-get install -y gnome-tweak-tool
-sudo apt-get install -y compiz compiz-gnome compiz-plugins compiz-plugins-extra compizconfig-settings-manager
-msg_c -c "Done!"
-
-msg_c -c "Install the ability to work with exfat drives"
-sudo apt-get install -y exfat-fuse exfat-utils
-msg_c -c "Done!"
-
-msg_c -c "install dconf editor for changing settings in gnome"
-sudo apt-get install -y dconf-editor
-
-## Install Sublime Text 3
-msg_c -c "Install SublimeText 3"
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-echo "deb https://download.sublimetext.com/apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get update && sudo apt-get install -y sublime-text
-msg_c -c "Done!"
-
-## Install php 7.1 (php7.2 is already out as of 2017-11-30 you should update to include it)
-msg_c -c "Install PHP"
-sudo apt-get install -y php7.1 \
-php7.1-cli \
-php7.1-curl \
-php7.1-common \
-php7.1-json \
-php7.1-mbstring \
-php7.1-gd \
-php7.1-intl \
-php7.1-xml \
-php7.1-mysql \
-php7.1-mcrypt \
-php7.1-zip
+msg_c -c "Installing dconf editor for changing settings in gnome"
+#----------------------------------------------------------------------------------------------------
+#sudo apt-get install -y dconf-editor
+#----------------------------------------------------------------------------------------------------
 msg_c -c "Done!"
 
 msg_c -c "Installing snaps"
+#----------------------------------------------------------------------------------------------------
 snap refresh
 snap install vlc qownnotes spotify gimp irccloud-desktop darktable inkscape
+#----------------------------------------------------------------------------------------------------
+msg_c -c "Done!"
+
+msg_c -c "Installing SublimeText 3"
+#----------------------------------------------------------------------------------------------------
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update && sudo apt-get install -y sublime-text
+#----------------------------------------------------------------------------------------------------
+msg_c -c "Done!"
+
+# Install php!
+msg_c -c "Installing php"
+#----------------------------------------------------------------------------------------------------
+install-php "7.4"
+#----------------------------------------------------------------------------------------------------
 msg_c -c "Done!"
 
 ## Install Composer
-cd "$HOME"
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
-sudo mv composer.phar /usr/local/bin/composer
+msg_c -c "Installing Composer"
+#----------------------------------------------------------------------------------------------------
+cd "$HOME" && install-php-composer
+#----------------------------------------------------------------------------------------------------
+msg_c -c "Done!"
 
+# Install node.js and npm
+msg_c -c "Installing nodejs and npm"
+#----------------------------------------------------------------------------------------------------
+cd "$HOME" && install-nodejs "14" "$platform"
+#----------------------------------------------------------------------------------------------------
+msg_c -c "Done!"
 
-## Install Python3 pip
-sudo apt-get install -y  python3-pip
+msg_c -c "Installing python3-pip"
+#----------------------------------------------------------------------------------------------------
+sudo apt-get install -y python3-pip
+#----------------------------------------------------------------------------------------------------
+msg_c -c "Done!"
 
-
-## Install node.js and npm
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt-get install -y nodejs
-
-
-## Set vim as the default editor
+# Set the default editor
 sudo update-alternatives --config editor
 
-
-## Create ssh keys
+# Create ssh keys
+msg_c -c "Create ssh keys"
+#----------------------------------------------------------------------------------------------------
 if [ ! -f "$HOME"/.ssh/id_rsa ]; then
     if [ ! -d "$HOME"/.ssh ]; then
         cd "$HOME" && mkdir .ssh
@@ -123,103 +130,30 @@ if [ ! -f "$HOME"/.ssh/id_rsa ]; then
 
     cd "$HOME"/.ssh && ssh-keygen -t rsa
 fi
+#----------------------------------------------------------------------------------------------------
+msg_c -c "Done!"
 
+msg_c -c "Install dotfiles"
+#----------------------------------------------------------------------------------------------------
+cd "$HOME" && install-dotfiles
+#----------------------------------------------------------------------------------------------------
+msg_c -c "Done!"
 
-## Set some Git config settings
-cd "$HOME"
-git config --global user.name "Joel Haker"
-git config --global user.email "joeladam@gmail.com"
-git config --global core.editor "vim"
-git config --global diff.tool vimdiff
-git config --global difftool.prompt false
-#git config --global alias.df diff
-#git config --global aliad.dt difftool
-#git config --global merge.tool vimdiff
-
-
-## Make Repos folder
-if [ ! -d "$HOME"/repos ]; then
-    cd "$HOME" && mkdir repos
-fi
-
-
-## Install myvimrc repo
-if [ ! -f "$HOME"/repos/myvimrc/.vimrc ]; then
-    echo ""
-    msg_c -g "Cloning the joeladam518/myvimrc github repo"
-    echo ""
-
-    if [ ! -d "$HOME"/repos ]; then
-        cd "$HOME" && mkdir repos
-    fi
-
-    if [ ! -d "$HOME"/repos/myvimrc ]; then
-        cd "$HOME"/repos && git clone "git@github.com:joeladam518/myvimrc.git"
-    fi
-
-    cd "$HOME" && ln -sf "$HOME"/repos/myvimrc/.vim
-    cd "$HOME" && ln -sf "$HOME"/repos/myvimrc/.vimrc
-
-    if [[ -f "$HOME"/repos/myvimrc/.vimrc &&  -e "$HOME"/.vimrc ]]; then
-        msg_c -g "Successfully installed the myvimrc repo"
-    else
-        msg_c -r "Something went wrong with installing the myvimrc repo"
-    fi
-else
-    msg_c -y ".vimrc is already installed"
-fi
-
-## Install mybashrc repo
-if [[ ! -f "$HOME"/.bashrc.old && -f "$HOME"/.bashrc ]]; then
-    echo ""
-    msg_c -g "Cloning the joeladam518/mybashrc github repo"
-
-    cd "$HOME" && mv .bashrc .bashrc.old
-
-    if [ -f "$HOME"/.bashrc.old ]; then
-        if [ ! -d "$HOME"/repos ]; then
-            cd "$HOME" && mkdir repos
-        fi
-
-        if [ ! -d "$HOME"/repos/mybashrc ]; then
-            cd "$HOME"/repos && git clone "git@github.com:joeladam518/mybashrc.git"
-        fi
-
-        cd "$HOME" && ln -sf "$HOME"/repos/mybashrc/desktop/.bashrc
-
-        if [[ -f "$HOME"/repos/mybashrc/server/.bashrc && -e "$HOME"/.bashrc ]]; then
-            msg_c -g "Successfully installed the mybashrc repo"
-        else
-            msg_c -r "Something went wrong with install mybashrc..."
-        fi
-    else
-        msg_c -r "Couldn't find .bashrc.old... Stopping what I'm doing..."
-    fi
-else
-    msg_c -y ".bashrc is already swapped out"
-fi
-
-## Install all your other repos
-echo ""
-msg_c -m "Now you can go ahead and install your other repos"
-echo ""
-
-## Install bash-git-prompt
 msg_c -c "Install bash-git-prompt"
+#----------------------------------------------------------------------------------------------------
 cd "$HOME" && git clone "https://github.com/magicmonty/bash-git-prompt.git" .bash-git-prompt --depth=1
+#----------------------------------------------------------------------------------------------------
 msg_c -c "Done!"
 
-
-## Install fzf
 msg_c -c "Install fzf"
-cd "$HOME" && git clone --depth 1 "https://github.com/junegunn/fzf.git" ~/.fzf
-cd "$HOME" && "$HOME"/.fzf/install
-set rtp+=~/.fzf
+#----------------------------------------------------------------------------------------------------
+cd "$HOME" && install-fzf
+#----------------------------------------------------------------------------------------------------
 msg_c -c "Done!"
 
 
-## Install your theme and icons
 msg_c -c "Install make the Ubuntu themes and icons folders"
+#----------------------------------------------------------------------------------------------------
 if [ ! -d "$HOME"/.themes ]; then
     cd "$HOME" && mkdir .themes
 fi
@@ -227,6 +161,7 @@ fi
 if [ ! -d "$HOME"/.icons ]; then
     cd "$HOME" && mkdir .icons
 fi
+#----------------------------------------------------------------------------------------------------
 msg_c -c "Done!"
 
 
