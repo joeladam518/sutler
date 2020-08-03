4c9cc47fb1dca0b29cf6398cf499433b2c4838d0
Joel Haker <joeladam@gmail.com>
2020-08-03 09:42:56 -0400

    git rid of the installer class

--- provisioner/app.py
+++ provisioner/app.py
@@ -1,26 +1,31 @@
 import click
-from provisioner.debian.installer import Installer
+from provisioner.debian import desktop, server
+from provisioner.config import os_types, machine_types
+from provisioner.utils import is_not_root
 
-
+# TODO: id your going to call this as root
 @click.group(invoke_without_command=True)
 def cli():
-    pass
+    if is_not_root():
+        raise click.ClickException('Provisioner must be run with administrator privileges.')
 
 
 @click.command()
 @click.argument('machine_type')
 @click.option('-o', '--os-type', 'os_type', type=str, default='debian', show_default=True)
-@click.option('-v', '--verbose', 'verbose', count=True)
-def install(machine_type, os_type, verbose):
-    click.secho('Install command called!', fg='cyan')
-    click.echo(f'machine_type: {machine_type}')
-    click.echo(f'     os_type: {os_type}')
-    click.echo(f'     verbose: {verbose}')
-    click.echo()
+def install(machine_type, os_type):
+    if os_type not in os_types:
+        raise click.ClickException('Invalid os type.')
 
-    installer = Installer(machine_type, os_type)
-    installer.validate()
-    installer.install()
+    if machine_type not in machine_types:
+        raise click.ClickException('Invalid machine type.')
+
+    if machine_type == 'desktop':
+        return desktop.install(os_type)
+    elif machine_type == 'server':
+        return server.install(os_type)
+
+    exit()
 
 
 cli.add_command(install)
