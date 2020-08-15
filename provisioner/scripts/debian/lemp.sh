f19c6cb8f07137887843216654c85fa397e23ad5
Joel Haker <joeladam@gmail.com>
2020-08-15 02:17:46 -0400

    remove script

--- provisioner/scripts/debian/lemp.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/scripts/env bash
-
-if ! ([ -n "$CWD" ] && [ -n "$platform" ] && [ "$provision_type" = "lemp" ]); then
-    echo "You can not call this file directly." 1>&2
-    exit 1
-fi
-
-# TODO: finish
