39675c7f8e6aca0960c184125773db8289a84081
Joel Haker <joeladam@gmail.com>
2020-08-14 23:08:41 -0400

    add singleton

---
+++ provisioner/application/singleton.py
@@ -0,0 +1,12 @@
+import os
+
+
+class SingletonMeta(type):
+    __instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls.__instances:
+            instance = super().__call__(*args, **kwargs)
+            cls.__instances[cls] = instance
+
+        return cls.__instances[cls]
