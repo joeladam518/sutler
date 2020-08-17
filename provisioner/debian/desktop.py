b30572273487e607a893521722d93b05a5ef0c2d
Joel Haker <joeladam@gmail.com>
2020-08-17 01:24:27 -0400

    WIP - add starter message on desktop and server

--- provisioner/debian/desktop.py
+++ provisioner/debian/desktop.py
@@ -2,4 +2,8 @@ import click
 
 
 def install(os_type):
+    click.secho('')
     click.echo(f'I will now install your {os_type} desktop machine.')
+
+    click.secho('Done!', fg='cyan')
+    click.secho('')
--- provisioner/debian/server.py
+++ provisioner/debian/server.py
@@ -2,4 +2,8 @@ import click
 
 
 def install(os_type):
-    click.echo(f'I will now install the basic server setup for a {os_type} machine.')
+    click.secho('')
+    click.secho(f'I will now install the basic server setup for a {os_type} machine.', fg='cyan')
+
+    click.secho('Done!', fg='cyan')
+    click.secho('')
