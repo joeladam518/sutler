fd17c9702f6994c1dd0a07c1b8b39ea32c13f8e4
Joel Haker <joeladam@gmail.com>
2020-08-22 00:01:55 -0400

    refactor

--- provisioner/utils.py
+++ provisioner/utils.py
@@ -1,11 +1,12 @@
 import os
+import click
 import ctypes
 import subprocess
 from provisioner.application import App
 
 
 def run_cmd():
-    pass
+    click.secho('Run Command!')
 
 
 def run_script(script, *args, as_root: bool = False):
@@ -31,8 +32,8 @@ def drop_privileges():
     os.setgroups([])
 
     # Try setting the new uid/gid
-    os.setgid(app.context.user.gid)
     os.setuid(app.context.user.uid)
+    os.setgid(app.context.user.gid)
 
     # Ensure a very conservative umask
     # 0o022 == 0755 for directories and 0644 for files
@@ -45,7 +46,6 @@ def is_root() -> bool:
         is_admin = os.getuid() == 0
     except AttributeError:
         is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
-
     return is_admin
 
 
