56dc274b28bb9d59a6c981e5fa99afbf6dbbfe49
Joel Haker <joeladam@gmail.com>
2021-12-21 00:45:54 -0500

    add a print of the description

--- sutler/commands/test.py
+++ sutler/commands/test.py
@@ -11,5 +11,8 @@ def context():
 
 @click.command()
 def test():
+    version_a = (8, 1)
+    version_b = '8.1.1'
+    print(f"Version({str(version_a)}) == Version({str(version_b)})")
     print(str(Version((8, 1)) == Version('8.1.0')))
     # print(Version('7.4'))
