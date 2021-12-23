168f74a430494ae815091e98ee860a7729c1c319
Joel Haker <joeladam@gmail.com>
2021-12-23 02:58:41 -0500

    add GitPython to dependencies

--- setup.py
+++ setup.py
@@ -7,8 +7,9 @@ setup(
     include_package_data=True,
     install_requires=[
         'Click',
+        'GitPython',
         'jinja2',
-        'packaging'
+        'packaging',
     ],
     entry_points='''
         [console_scripts]
