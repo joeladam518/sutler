959e98593da72326d48951811868c50b1914b4f3
Joel Haker <joeladam@gmail.com>
2021-12-24 20:47:32 -0500

    use the scripts path helper

--- sutler/installers/composer.py
+++ sutler/installers/composer.py
@@ -4,8 +4,7 @@ from .installer import Installer
 
 class ComposerInstaller(Installer):
     def install(self):
-        scripts_path = self.app.get_path('scripts')
-        Run.script(f"{scripts_path}/install-php-composer")
+        Run.script(self.app.scripts_path('install-php-composer'))
 
     def uninstall(self):
         pass
