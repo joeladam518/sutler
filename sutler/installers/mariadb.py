b0e17a34198e84f772420ab4724ca5aad28f405b
Joel Haker <joeladam@gmail.com>
2021-12-26 22:41:04 -0500

    WIP: start the maria db script

--- sutler/installers/mariadb.py
+++ sutler/installers/mariadb.py
@@ -1,9 +1,18 @@
+import os
+import shutil
+from ..application import Run
 from .installer import Installer
 
 
 class MariadbInstaller(Installer):
     def install(self) -> None:
-        pass
+        Run.install('mariadb-server', 'mariadb-client')
+        Run.command('mysql_secure_installation', root=True)
+
+        # Setup Mariadb to support utf-8
+        src_path = self.app.templates_path('mysql', 'utf8.conf')
+        dst_path = os.path.join('/etc', 'mysql', 'conf.d', 'utf8.conf')
+        Run.command(f'cp "{src_path}" "{dst_path}"', root=True)
 
     def uninstall(self) -> None:
-        pass
+        Run.uninstall('mariadb-server', 'mariadb-client')
