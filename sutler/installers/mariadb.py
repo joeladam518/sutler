7147fc02b743c9b68707cf877f1eb030d548bbf0
Joel Haker <joeladam@gmail.com>
2021-12-26 22:55:12 -0500

    stub more stuff

--- sutler/installers/mariadb.py
+++ sutler/installers/mariadb.py
@@ -1,23 +1,32 @@
+import click
 import os
 from ..application import Run
 from .installer import Installer
-
-
-def create_db(name: str, user_name: str, user_password: str) -> None:
-    pass
 
 
 class MariadbInstaller(Installer):
     def install(self) -> None:
         Run.install('mariadb-server', 'mariadb-client')
 
-        # Secure the install
-        Run.command('mysql_secure_installation', root=True)
-
         # Setup Mariadb to support utf-8
         src_path = self.app.templates_path('mysql', 'utf8.conf')
         dst_path = os.path.join('/etc', 'mysql', 'conf.d', 'utf8.conf')
         Run.command(f'cp "{src_path}" "{dst_path}"', root=True)
+
+        # Secure the install
+        click.echo()
+        Run.command('mysql_secure_installation', root=True)
+        click.echo()
+
+        if not click.confirm('Do you want to create a new database right now?', default=None):
+            # We're done!
+            return
+
+        click.echo()
+        db_root_password = click.prompt('What is you mariadb root password?')
+        db_name = click.prompt("The name of the new db?")
+        db_user = click.prompt("The name of the db's primary user?")
+        db_user_password = click.prompt('Their intended password?')
 
         # TODO: create a new database
 
