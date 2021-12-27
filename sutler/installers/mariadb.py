007be6e44abcadaaae2b4eef8df7c26ca5eafbeb
Joel Haker <joeladam@gmail.com>
2021-12-26 22:46:33 -0500

    stub out more

--- sutler/installers/mariadb.py
+++ sutler/installers/mariadb.py
@@ -1,18 +1,25 @@
 import os
-import shutil
 from ..application import Run
 from .installer import Installer
+
+
+def create_db(name: str, user_name: str, user_password: str) -> None:
+    pass
 
 
 class MariadbInstaller(Installer):
     def install(self) -> None:
         Run.install('mariadb-server', 'mariadb-client')
+
+        # Secure the install
         Run.command('mysql_secure_installation', root=True)
 
         # Setup Mariadb to support utf-8
         src_path = self.app.templates_path('mysql', 'utf8.conf')
         dst_path = os.path.join('/etc', 'mysql', 'conf.d', 'utf8.conf')
         Run.command(f'cp "{src_path}" "{dst_path}"', root=True)
+
+        # TODO: create a new database
 
     def uninstall(self) -> None:
         Run.uninstall('mariadb-server', 'mariadb-client')
