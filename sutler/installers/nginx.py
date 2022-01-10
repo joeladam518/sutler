e86e3b2baa69cc84a46fbb6d6098c4499fb4dfec
Joel Haker <joeladam@gmail.com>
2022-01-09 21:42:05 -0500

    changes to config files to make everything work.

--- sutler/installers/nginx.py
+++ sutler/installers/nginx.py
@@ -1,30 +1,76 @@
 import click
 import os
+from typing import Any
 from ..application import Run
 from .installer import Installer
 
 
 class NginxInstaller(Installer):
-    def install(self) -> None:
+    def _render_file(self, tp: str, fp: str, root: bool = False, **variables: Any) -> None:
+        """
+        :param str tp: Template path
+        :param str fp: File Path
+        :param Any variables: The variables for the template
+        :return: None
+        :rtype: None
+        """
+        tmp_dir = self.app.sys_path('tmp', 'sutler-templates')
+        filename = os.path.basename(fp)
+
+        if not os.path.exists(tmp_dir):
+            os.makedirs(tmp_dir)
+
+        tmp_file_path = os.path.join(tmp_dir, filename)
+        template = self.app.jinja.get_template(tp)
+        template.stream(variables).dump(tmp_file_path, 'utf-8')
+
+        Run.command(f"mv {tmp_file_path} {fp}", root=root)
+        if root:
+            Run.command(f"chown root:root {fp}", root=True)
+
+    def install(self, project: str, domain: str, php_version: str) -> None:
         Run.update()
         Run.install('nginx')
-
-        # Run.command("ufw allow 'Nginx HTTP'", root=True)
-        # Run.command("ufw allow 'Nginx HTTPS'", root=True)
-        # click.echo()
-        # click.echo('Checking ufw status:')
-        # Run.command("ufw status", root=True)
-        # click.echo()
-
         Run.command('systemctl stop nginx', root=True)
 
-        nginx_conf_path = os.path.join(os.sep, 'etc', 'nginx', 'nginx.conf')
-        if os.path.exists(nginx_conf_path):
-            Run.command(f'mv {nginx_conf_path} {nginx_conf_path}.old', root=True)
-        self.app.render_file(
+        # make the directory for the project
+        project_path = self.app.sys_path('var', 'www', project)
+        Run.command(f"mkdir -p {project_path}", root=True)
+        Run.command(f"mkdir -p {project_path}/public", root=True)
+        Run.command(f"chown -r www-data:www-data {project_path}", root=True)
+
+        # Build all the config paths
+        etc_path = self.app.sys_path('etc', 'nginx')
+        config_path = os.path.join(etc_path, 'nginx.conf')
+        sites_available_path = os.path.join(etc_path, 'sites-available')
+        sites_enabled_path = os.path.join(etc_path, 'sites-enabled')
+        project_config_path = os.path.join(sites_available_path, project)
+
+        # let's keep the old nginx config path
+        if os.path.exists(config_path):
+            Run.command(f'mv {config_path} {config_path}.old', root=True)
+
+        # render the template files to their config paths
+        self._render_file(
             os.path.join('nginx', 'nginx.conf.jinja2'),
-            nginx_conf_path
+            config_path,
+            root=True
         )
+        self._render_file(
+            os.path.join('nginx', 'sites-available', 'php-http.nginx.jinja2'),
+            project_config_path,
+            root=True,
+            domain=domain,
+            project_name=project,
+            php_version=php_version,
+            gzip_config=self.app.jinja.get_template('nginx/snippets/compression.conf.jinja2').render()
+        )
+
+        # enable the nginx project site
+        os.chdir(sites_enabled_path)
+        Run.command(f"rm {sites_enabled_path}/*", root=True)
+        Run.command(f"ln -s {project_config_path}", root=True)
+        Run.command('systemctl start nginx', root=True)
 
     def uninstall(self) -> None:
         Run.uninstall('nginx')
--- sutler/provisioners/lemp.py
+++ sutler/provisioners/lemp.py
@@ -7,6 +7,10 @@ from .server import ServerProvisioner
 
 class LempProvisioner(ServerProvisioner):
     def run(self) -> None:
+        project = 'sutler-lemp'
+        domain = 'ubuntu.sutler.test'
+        php_version = '8.1'
+
         super().run()
 
         click.echo()
@@ -22,10 +26,18 @@ class LempProvisioner(ServerProvisioner):
         installer.install('16')
 
         installer = PhpInstaller(self.ctx)
-        installer.install('8.1', env='server')
+        installer.install(php_version, env='server')
 
         installer = ComposerInstaller(self.ctx)
         installer.install()
 
         installer = NginxInstaller(self.ctx)
-        installer.install()
+        installer.install(project, domain, php_version)
+
+        # TODO: Configure ufw
+        # Run.command("ufw allow 'Nginx HTTP'", root=True)
+        # Run.command("ufw allow 'Nginx HTTPS'", root=True)
+        # click.echo()
+        # click.echo('Checking ufw status:')
+        # Run.command("ufw status", root=True)
+        # click.echo()
