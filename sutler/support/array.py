24708430c075330b75e3ce6929e5e095a689f2ba
Joel Haker <joeladam@gmail.com>
2022-01-17 00:26:42 -0500

    convert the list helper in to something the can accept lists of tuples

---
+++ sutler/support/array.py
@@ -0,0 +1,24 @@
+from collections import OrderedDict
+from typing import Union
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Public interface
+
+ArrType = Union[list, tuple]
+
+
+class Arr:
+    @staticmethod
+    def exclude(items: ArrType, _exclude: ArrType) -> ArrType:
+        original_type = type(items)
+        return original_type([item for item in items if item not in _exclude])
+
+    @staticmethod
+    def only(items: ArrType, _only: ArrType) -> ArrType:
+        original_type = type(items)
+        return original_type([item for item in items if item in _only])
+
+    @staticmethod
+    def unique(items: ArrType) -> ArrType:
+        original_type = type(items)
+        return original_type(OrderedDict.fromkeys(items))
