230ba4a92d13d0ec29cb0e69aa94001565d1c2f4
Joel Haker <joeladam@gmail.com>
2022-01-27 01:06:15 -0500

    add space

--- sutler/installers/sumblime.py
+++ sutler/installers/sumblime.py
@@ -12,6 +12,7 @@ class SublimeInstaller(Installer):
 
         self.app.os.exec('wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -')
         self.app.os.exec(f'echo "{self.__source}" | sudo tee {self.__source_file_path}', root=True)
+
         self.app.os.update()
         self.app.os.install(f'sublime-{program}')
 
