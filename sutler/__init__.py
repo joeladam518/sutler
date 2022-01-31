47bea5ddd78194444f2444c0a517037172dc187e
Joel Haker <joeladam@gmail.com>
2022-01-30 20:47:46 -0500

    suppress git python errors

--- sutler/__init__.py
+++ sutler/__init__.py
@@ -1,1 +1,6 @@
 # -*- coding: utf-8 -*-
+import os
+
+# Supress error message if git isn't installed yet.
+# Hopefully, it will be installed by the time we need it.
+os.environ['GIT_PYTHON_REFRESH'] = 'quiet'
