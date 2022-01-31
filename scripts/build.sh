f6f0f3dae81888d911781151c2fdbd72a80cf8f9
Joel Haker <joeladam@gmail.com>
2022-01-31 01:00:47 -0500

    make executable

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,21 @@
 SCRIPTS_DIR="$(cd "$(dirname "$0")" > /dev/null 2>&1 && pwd -P)"
 PROJECT_DIR="$(dirname "$SCRIPTS_DIR")"
 
+if [ -n "${SETUP_VIRTUALENV:-""}" ]; then
+    if command -v "virtualenv" >/dev/null 2>&1; then
+        virtualenv venv
+    elif python3 -c "import venv" >/dev/null 2>&1; then
+        python3 -m venv venv
+    else
+        echo "Can not create a virtual environment." 2>&1
+        exit 1
+    fi
+
+    source venv/bin/activate
+fi
+
+cd "${PROJECT_DIR}" && pip install -r requirements.txt pyinstaller
+
 pyinstaller \
   --noconfirm \
   --log-level=WARN \
@@ -11,3 +26,7 @@ pyinstaller \
   --nowindow \
   --name=sutler \
   "${PROJECT_DIR}/sutler.spec"
+
+if [ -n "${SETUP_VIRTUALENV:-""}" ]; then
+    deactivate
+fi
